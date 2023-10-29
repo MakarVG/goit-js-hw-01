36fc70116f64986e48206185695027a780ad8338
Makar91 <makarenko.vlad@gmail.com>
2023-10-29 13:24:25 +0200

    Upd 1

---
+++ js/task-3.js
@@ -0,0 +1,11 @@
+const getElementWidth = (content, padding, border) => {
+    const paddingValue = parseFloat(padding);
+    const borderValue = parseFloat(border);
+    const contentValue = parseFloat(content);
+
+    return contentValue + borderValue * 2 + paddingValue * 2;
+};
+
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
